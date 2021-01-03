011c25266023dce0ddd0a9b47373fa3a2700c710
jeffreytjs <jeffreytjs1996@gmail.com>
2021-01-03 21:41:53 +0800

    Add more tiers to upgrade for testing

--- Catapult/Game/Upgrades/UnlockedUpgrades.gd
+++ Catapult/Game/Upgrades/UnlockedUpgrades.gd
@@ -4,7 +4,7 @@ signal upgraded
 signal change_money
 
 #why is the money var here? idk
-var money = 0
+var money = 50000
 
 var upgrades = {
 	#there were gonna be more cats, but I got too lazy to add them.
@@ -13,43 +13,57 @@ var upgrades = {
 		{"unlocked": false, "price": 50, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")},
 		{"unlocked": false, "price": 100, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")},
 		{"unlocked": false, "price": 200, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")},
+		{"unlocked": false, "price": 300, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")},
+		{"unlocked": false, "price": 300, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")},
 		{"unlocked": false, "price": 300, "value": preload("res://Game/World/Cats/Olin/Olin.tscn")}
 	],
 	"milk_spawn_chance": [
 		{"unlocked":true, "price": 0, "value": 0.0015},
 		{"unlocked":false,"price": 225, "value": 0.0025},
 		{"unlocked":false,"price": 350, "value": 0.0035},
-		{"unlocked":false,"price": 450, "value": 0.0045}
+		{"unlocked":false,"price": 450, "value": 0.0045},
+		{"unlocked":false,"price": 350, "value": 0.0055},
+		{"unlocked":false,"price": 450, "value": 0.0065}
 	],
 	"money_spawn_chance": [
 		{"unlocked":true, "price": 0, "value": 0.03},
-		{"unlocked":false,"price": 175, "value": 0.05},
-		{"unlocked":false,"price": 350, "value": 0.07},
-		{"unlocked":false,"price": 600, "value": 0.09}
+		{"unlocked":false,"price": 17, "value": 0.05},
+		{"unlocked":false,"price": 35, "value": 0.07},
+		{"unlocked":false,"price": 60, "value": 0.09},
+		{"unlocked":false,"price": 17, "value": 0.11},
+		{"unlocked":false,"price": 35, "value": 0.13},
 	],
 	"trampoline_strength": [
 		{"unlocked":true, "price": 0, "value": 1.1},
 		{"unlocked":false,"price": 150, "value": 1.2},
 		{"unlocked":false,"price": 300, "value": 1.3},
-		{"unlocked":false,"price": 550, "value": 1.4}
+		{"unlocked":false,"price": 550, "value": 1.4},
+		{"unlocked":false,"price": 300, "value": 1.5},
+		{"unlocked":false,"price": 550, "value": 1.6}
 	],
 	"catapult_height": [
 		{"unlocked":true, "price": 0, "value": 135},
 		{"unlocked":false,"price": 75, "value": 127},
 		{"unlocked":false,"price": 175, "value": 120},
-		{"unlocked":false,"price": 275, "value": 115}
+		{"unlocked":false,"price": 275, "value": 115},
+		{"unlocked":false,"price": 175, "value": 110},
+		{"unlocked":false,"price": 275, "value": 103}
 	],
 	"catapult_strength": [
 		{"unlocked":true, "price": 0, "value": 1.1},
 		{"unlocked":false,"price": 250, "value": 1.35},
 		{"unlocked":false,"price": 400, "value": 1.75},
-		{"unlocked":false,"price": 650, "value": 2.0}
+		{"unlocked":false,"price": 650, "value": 2.0},
+		{"unlocked":false,"price": 400, "value": 2.35},
+		{"unlocked":false,"price": 650, "value": 2.70}
 	],
 	"magnet_strength": [
 		{"unlocked":true, "price": 0, "value": 1.0},
-		{"unlocked":false,"price": 50, "value": 2.0},
-		{"unlocked":false,"price": 175, "value": 3.0},
-		{"unlocked":false,"price": 400, "value": 5.0}
+		{"unlocked":false,"price": 5, "value": 2.0},
+		{"unlocked":false,"price": 17, "value": 3.0},
+		{"unlocked":false,"price": 40, "value": 5.0},
+		{"unlocked":false,"price": 17, "value": 8.0},
+		{"unlocked":false,"price": 40, "value": 10.0}
 	]
 }
 
@@ -100,4 +114,3 @@ func reset():
 		for i in upgrades[k].size():
 			if i != 0:
 				upgrades[k][i].unlocked = false
-			
